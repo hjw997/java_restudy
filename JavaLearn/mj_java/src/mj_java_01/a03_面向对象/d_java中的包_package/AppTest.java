f47bfbf584983e1e58723162df8b1b381794b8c5
whj <wanghaijun.425@163.com>
2023-12-01 01:01:15 +0800

    add: mj_java01 (java的包的相关内容)

---
+++ JavaLearn/mj_java/src/mj_java_01/a03_面向对象/d_java中的包_package/AppTest.java
@@ -0,0 +1,10 @@
+package mj_java_01.a03_面向对象.d_java中的包_package; /**类的第一句必须使用 package 声明自己属于哪个包*/
+
+/**下面的两个是编译器自动导入 ,可省略*/
+import java.lang.*; /**java开发中的最常用的一些类型 */
+import mj_java_01.a03_面向对象.d_java中的包_package.*; /**源文件所在的包*/
+public class AppTest {
+    public static void main(String[] args) {
+        String str = "hahah";
+    }
+}
