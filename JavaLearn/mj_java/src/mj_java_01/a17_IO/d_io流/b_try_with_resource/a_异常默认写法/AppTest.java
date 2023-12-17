6ea591b99b5355cabc54fc1962915cbc4929163a
whj <wanghaijun.425@163.com>
2023-12-18 02:40:23 +0800

    add: mj_java 01 ( try-with-resource 资源的异常捕获 简化代码. )

---
+++ JavaLearn/mj_java/src/mj_java_01/a17_IO/d_io流/b_try_with_resource/a_异常默认写法/AppTest.java
@@ -0,0 +1,19 @@
+package mj_java_01.a17_IO.d_io流.b_try_with_resource.a_异常默认写法;
+
+/**
+ * try-with-resouces 语句: 看课件:
+ * try(资源1;资源2;资源3;...) {
+ *
+ * } catch (Exception e) {
+ *
+ * } finally {
+ *
+ * }
+ *
+ * PS可以 对比 a 包和 b 包中 两个 Files 工具类 .
+ */
+public class AppTest {
+    public static void main(String[] args) {
+
+    }
+}
