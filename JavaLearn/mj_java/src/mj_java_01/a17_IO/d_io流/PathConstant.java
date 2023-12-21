539f60f3a8022783701eb5048b785177f5f9da38
whj <wanghaijun.425@163.com>
2023-12-22 00:13:43 +0800

    add: mj_java 01 ( path 修改)

--- JavaLearn/mj_java/src/mj_java_01/a17_IO/d_io流/PathConstant.java
+++ JavaLearn/mj_java/src/mj_java_01/a17_IO/d_io流/PathConstant.java
@@ -4,5 +4,5 @@ public class PathConstant {
     /**
      * IO 流测试的 根目录
      */
-    public static final String ioTestRootPath = "/Users/amos/Desktop/io_test/";
+    public static final String ioTestRootPath = "/Users/whj/Desktop/io_test/";
 }
