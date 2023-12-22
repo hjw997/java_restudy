759a7e170ad5edcc460dd7015cec7f5e2d9f4878
whj <wanghaijun.425@163.com>
2023-12-22 11:30:34 +0800

    add: mj_java 01 ( Scanner 类 )

---
+++ JavaLearn/mj_java/src/mj_java_01/a17_IO/d_io流/e_Scanner_适合控制台获取数据_初学java/AppTest.java
@@ -0,0 +1,9 @@
+package mj_java_01.a17_IO.d_io流.e_Scanner_适合控制台获取数据_初学java;
+
+/**
+ * mj视频:
+ * https://www.bilibili.com/video/BV1F44y1r7cv?p=108&spm_id_from=pageDriver&vd_source=9d2ff4daa93ba2f65d04081137bf4e05
+ *
+ */
+public class AppTest {
+}
