866e40c14e244124f0812733ab8bc69663f237a7
whj <wanghaijun.425@163.com>
2023-12-25 04:03:01 +0800

    add: mj_java 01 ( 泛型限制类型参数-睡觉了 )

---
+++ JavaLearn/mj_java/src/mj_java_01/a15_泛型_Generic/c_限制类型参数/b_限制类型参数_示例/AppTest.java
@@ -0,0 +1,8 @@
+package mj_java_01.a15_泛型_Generic.c_限制类型参数.b_限制类型参数_示例;
+
+public class AppTest {
+    public static void main(String[] args) {
+        //https://www.bilibili.com/video/BV1F44y1r7cv?p=57&spm_id_from=pageDriver&vd_source=9d2ff4daa93ba2f65d04081137bf4e05
+        // P57 3分:
+    }
+}
