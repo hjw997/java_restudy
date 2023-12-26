c17b597b39917dc79d45396465f62860f2240458
whj <wanghaijun.425@163.com>
2023-12-27 02:28:34 +0800

    add: mj_java 01 ( 泛型学习使用限制  )

--- JavaLearn/mj_java/src/mj_java_01/a15_泛型_Generic/h_泛型使用限制/AppTest.java
+++ JavaLearn/mj_java/src/mj_java_01/a15_泛型_Generic/h_泛型使用限制/AppTest.java
@@ -1,9 +1,21 @@
 package mj_java_01.a15_泛型_Generic.h_泛型使用限制;
 
-public class AppTest {
+import java.util.HashMap;
+import java.util.Map;
 
+public class AppTest {
     public static void main(String[] args) {
-        //https://www.bilibili.com/video/BV1F44y1r7cv?p=62&spm_id_from=pageDriver&vd_source=9d2ff4daa93ba2f65d04081137bf4e05
-        //p62 再看一遍. 有需要补充
+        // p62 https://www.bilibili.com/video/BV1F44y1r7cv?p=62&spm_id_from=pageDriver&vd_source=9d2ff4daa93ba2f65d04081137bf4e05
+
+
+        //1.基本类型 不能作为 类型参数
+        //Map<int, char> map1 = new HashMap<>();
+
+        //不能 类似 new E();
+
+
     }
+
+
 }
+
