5d3effdee495d33fc7e4603334287989fd5f863d
whj <wanghaijun.425@163.com>
2024-01-02 01:38:54 +0800

    add: mj_java 01 ( 正则表达式 )

--- JavaLearn/mj_java/src/mj_java_01/a14_正则表达式/c_量词_Quantifier/AppTest.java
+++ JavaLearn/mj_java/src/mj_java_01/a14_正则表达式/c_量词_Quantifier/AppTest.java
@@ -74,9 +74,11 @@ public class AppTest {
     public void test03() {
         /**
          * [abc]{3} 对于这个的理解
-         * [abc] 表示[abc] a,b,c 中的任意一个,x{3} ,表示x出现3次. 不要以为是 aaa bbb ccc .
+         * [abc] 表示[abc] a,b,c 中的任意一个,x{3} ,表示x出现3次. 不要以为是 aaa bbb ccc . ❌
          * 正确理解✅: X{3} 表示X出现三次,所以 [abc]{3} 表示:[abc][abc][abc] 这样子出现3次.
          * 所以以后看到 [abc]{3} 右边有量词 [abc]{3} 的.记得 如:X{3} X整体 就出现3次.
+         *
+         * [abc] 是单字符.
          */
         String regex = "[abc]{3}"; //[abc][abc][abc]--每一处地方都是 a,b,c 的任意一个.
         String input = "abccabaaaccbbbc";
@@ -106,7 +108,7 @@ public class AppTest {
     @Test
     public void test05() {
         String input = "a";
-        findAll("a?",input); //a匹配,然后去掉a 用""匹配. 出现0次a 所以也匹配.
+        findAll("a?",input); //a匹配,然后去掉a 剩下的字符"" 匹配. 出现0次a 所以也匹配.
         /**
          "a": [0,1)
          "": [1,1)
@@ -117,6 +119,11 @@ public class AppTest {
          * x* x出现任意次,0次 1次. 2次.
          "a": [0,1)
          "": [1,1)
+         */
+
+        findAll("a+",input); // 至少出现一次
+        /**
+         "a": [0,1)
          */
 
     }
